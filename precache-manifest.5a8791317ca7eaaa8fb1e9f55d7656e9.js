052c26592238ccd0c905d5595b0fde60c5c1799c
Shalom Sam <shalom.s@endurance.com>
2019-03-01 19:12:40 +0530

    Updates

---
+++ precache-manifest.5a8791317ca7eaaa8fb1e9f55d7656e9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "9970e085c9eeedea109d9c30be68115d",
+    "url": "/react-canvas-js/static/media/icon-bootstrap.9970e085.png"
+  },
+  {
+    "revision": "67891e14bbd4d3d635d8",
+    "url": "/react-canvas-js/static/css/main.94a53b3f.chunk.css"
+  },
+  {
+    "revision": "18ffea4c3cb58edb9968",
+    "url": "/react-canvas-js/static/js/runtime~main.18ffea4c.js"
+  },
+  {
+    "revision": "c500a3801d8356a86da86a06c3d13a4d",
+    "url": "/react-canvas-js/static/media/icon-webpack.c500a380.png"
+  },
+  {
+    "revision": "410ed63d26a710714eb1",
+    "url": "/react-canvas-js/static/js/2.410ed63d.chunk.js"
+  },
+  {
+    "revision": "67891e14bbd4d3d635d8",
+    "url": "/react-canvas-js/static/js/main.67891e14.chunk.js"
+  },
+  {
+    "revision": "0109fced2b72718e149bac591a34ac49",
+    "url": "/react-canvas-js/static/media/icon-github.0109fced.png"
+  },
+  {
+    "revision": "fb715d72022219140fce8e3931088bbf",
+    "url": "/react-canvas-js/static/media/icon-nodejs.fb715d72.png"
+  },
+  {
+    "revision": "90552fc53de18398a934d32aec962234",
+    "url": "/react-canvas-js/static/media/icon-react.90552fc5.png"
+  },
+  {
+    "revision": "410ed63d26a710714eb1",
+    "url": "/react-canvas-js/static/css/2.3d98027c.chunk.css"
+  },
+  {
+    "revision": "25979ccd92e88052b7ce8274a032a47d",
+    "url": "/react-canvas-js/index.html"
+  }
+];
